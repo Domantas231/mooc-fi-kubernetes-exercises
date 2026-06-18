123d767df11021b2c3d233cd406a7a61a74aeb8e
Domantas Bieliūnas <domantas231@gmail.com>
2026-06-18 21:24:48 +0300

    1.5

--- the_project/index.js
+++ the_project/index.js
@@ -2,9 +2,28 @@ const http = require('http');
 
 const PORT = process.env.PORT || 3000;
 
+const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Todo app</title>
+</head>
+<body>
+  <h1>Hello world</h1>
+</body>
+</html>
+`;
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Todo app\n');
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(html);
+    return;
+  }
+
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.end('Not found\n');
 });
 
 server.listen(PORT, () => {
